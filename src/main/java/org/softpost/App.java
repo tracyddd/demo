a064369e44ef33749a6755dc8ed3b535eed13943
Tracy <tracychrishu@teksavvy.com>
2020-12-24 19:28:06 -0500

    test

---
+++ src/main/java/org/softpost/App.java
@@ -0,0 +1,15 @@
+package org.softpost;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World! This is from softpost app" );
+        System.out.println( "this is to test github" );
+    }
+}
