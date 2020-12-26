e5846d761ac103c09fb327e338388568e90aa1b4
TD <76573024+tracyddd@users.noreply.github.com>
2020-12-25 20:13:51 -0500

    Update AppTest.java

--- src/test/java/test/java/AppTest.java
+++ src/test/java/test/java/AppTest.java
@@ -22,7 +22,7 @@ public class AppTest{
     public void setUp() throws Exception {
         System.setProperty("webdriver.chrome.driver", "C:\\SeleniumWebdrivers\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        String baseUrl = "http://www.V0.com/";
+        String baseUrl = "http://www.V2.com/";
         driver.get(baseUrl);
         driver.quit();
     }
